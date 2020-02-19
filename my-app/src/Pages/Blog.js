a2f99e43fcc63ed916d5a7438b8e15b8d850aca3
RomanButsiy <butsiyroman@gmail.com>
2020-02-19 23:05:05 +0200

    Added static blog

--- my-app/src/Pages/Blog.js
+++ my-app/src/Pages/Blog.js
@@ -1,11 +1,111 @@
 import React, { Component } from 'react';
+import { Card, Row, Col, Media, Container, ListGroup } from 'react-bootstrap';
 
 export default class Blog extends Component {
     render() {
         return (
-            <div>
-                Hello Blog
-            </div>
+            <Container>
+                <Row>
+                    <Col md="9">
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+                                alt="js"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                            </Media.Body>
+                        </Media>
+                    </Col>
+                    <Col md={3}>
+                        <h5 className="text-center mt-4">Categories</h5>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Html/Css</ListGroup.Item>
+                                <ListGroup.Item>JavaScript</ListGroup.Item>
+                                <ListGroup.Item>Python</ListGroup.Item>
+                                <ListGroup.Item>Java</ListGroup.Item>
+                                <ListGroup.Item>C++</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                        <Card className="mt-3" bg="light">
+                            <Card.Body>
+                                <Card.Title>Site widget</Card.Title>
+                                <Card.Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
