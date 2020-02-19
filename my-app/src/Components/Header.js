c3d92d38a935992d4cad96e6a6c8faad5f5a716b
RomanButsiy <butsiyroman@gmail.com>
2020-02-19 17:00:40 +0200

    Added route

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, FormControl, Form, Button, Container } from 'react-bootstrap';
 import logo from './logo192.png';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Contacts from '../Pages/Contacts';
+import Blog from '../Pages/Blog';
 
 export default class Header extends Component {
     render() {
         return (
             <>
-                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark" >
+                <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" >
                     <Container>
                         <Navbar.Brand href="/">
                             <img
@@ -36,6 +41,15 @@ export default class Header extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+                
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/contacts" component={Contacts} />
+                        <Route exact path="/blog" component={Blog} />
+                    </Switch>
+                </Router>
             </>
         )
     }
