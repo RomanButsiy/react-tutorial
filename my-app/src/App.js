74b3ac088d0a8b299d6324b89dd28584aa330412
RomanButsiy <butsiyroman@gmail.com>
2020-02-19 15:55:16 +0200

    Init project

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello world </h1>
+      <Button variant="primary">Init Button</Button>
+    </div>
+  );
+}
+
+export default App;
