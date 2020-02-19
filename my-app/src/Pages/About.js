523409f0921573d77c4546faff68f70a24171fdf
RomanButsiy <butsiyroman@gmail.com>
2020-02-19 22:35:47 +0200

    Added CardDeck

--- my-app/src/Pages/About.js
+++ my-app/src/Pages/About.js
@@ -29,24 +29,24 @@ export default class About extends Component {
                         <Col sm={9}>
                             <Tab.Content className="mt-3">
                                 <Tab.Pane eventKey="first">
-                                    <img src="https://cms-assets.tutsplus.com/uploads/users/30/posts/32053/image/figma_themes.png" alt="Design" />
+                                    <img src="https://cms-assets.tutsplus.com/uploads/users/30/posts/32053/image/figma_themes.png" alt="Design" className="d-block w-100" />
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">
-                                    <img src="https://i.pinimg.com/originals/6f/6d/f1/6f6df138c190cc2c31e9fd5564e4cf6b.png" alt="Team" />
+                                    <img src="https://i.pinimg.com/originals/6f/6d/f1/6f6df138c190cc2c31e9fd5564e4cf6b.png" alt="Team" className="d-block w-100" />
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="third">
-                                    <img src="https://raw.githubusercontent.com/RomanButsiy/StartFP100/master/screens/Screen1.png" alt="Programing " width="auto" height="500"/>
+                                    <img src="https://raw.githubusercontent.com/RomanButsiy/StartFP100/master/screens/Screen1.png" alt="Programing "  className="d-block w-100" />
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="fourth">
-                                    <img src="https://cdn-media-1.freecodecamp.org/images/1*9M_Anr2z0eC8AT0adn52Nw.png" alt="Frameworks" />
+                                    <img src="https://cdn-media-1.freecodecamp.org/images/1*9M_Anr2z0eC8AT0adn52Nw.png" alt="Frameworks" className="d-block w-100" />
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="fifth">
-                                    <img src="https://coderseye.com/wp-content/uploads/Jquery-Libraries-vs-Frameworks-vs-APIS.jpg" alt="Libraries" />
+                                    <img src="https://coderseye.com/wp-content/uploads/Jquery-Libraries-vs-Frameworks-vs-APIS.jpg" alt="Libraries" className="d-block w-100"/>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                 </Tab.Pane>
                             </Tab.Content>
--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
 import CarouselBox from '../Components/CarouselBox';
+import { Container, CardDeck, Card, Button } from 'react-bootstrap';
 
 export default class Home extends Component {
     render() {
         return (
-            <CarouselBox />
+            <>
+                <CarouselBox />
+                <Container>
+                    <h2 className="text-center m-4">Our team</h2>
+                    <CardDeck className="m-4">
+                        <Card>
+                            <Card.Img 
+                                variant="top"
+                                src="https://images.fineartamerica.com/images-medium-large-5/success-puzzle-agedpixel.jpg"
+                                alt="img_1"
+                            />
+                            <Card.Body>
+                                <Card.Title>Puzzle</Card.Title>
+                                <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Connected</Card.Title>
+                                <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                            <Card.Img 
+                                variant="bottom"
+                                src="https://images.fineartamerica.com/images-medium-large-5/abstract-artwork-with-connected-spheres-pasieka.jpg"
+                                alt="img_2"
+                            />
+                        </Card>
+                        <Card>
+                            <Card.Img 
+                                variant="top"
+                                src="https://images.fineartamerica.com/images-medium-large-5/human-networking-across-the-globe-fanatic-studio--science-photo-library.jpg"
+                                alt="img_3"
+                            />
+                            <Card.Body>
+                                <Card.Title>Networking</Card.Title>
+                                <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                </Container>
+            </>
+
         )
     }
 }
